ec1d5c77af6537feab05d296ae671d5b026bec73
Jorge Echavarria <jorge.echavarria@lrz.de>
2023-08-22 15:26:09 +0000

    Minor changes

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,29 +5,29 @@
 using namespace llvm;
 
 int main() {
-	ModuleAnalysisManager MAM;
+    ModuleAnalysisManager MAM;
     QirPassManager        QPM;
     LLVMContext           Context;
     SMDiagnostic          error;
 
-   	// Read QIR 
+    // Read QIR 
     std::unique_ptr<Module> module = parseIRFile("../benchmarks/bell_state.ll", error, Context);
     if (!module) {
         std::cerr << "Error reading .ll file." << std::endl;
         return 1;
     }
 
-	// Append passes
+    // Append passes
     QPM.append("./src/passes/libQirRemoveNonEntrypointFunctionsPass.so");
     QPM.append("./src/passes/libQirGroupingPass.so");
     QPM.append("./src/passes/libQirBarrierBeforeFinalMeasurementsPass.so");
     QPM.append("./src/passes/libQirCXCancellationPass.so");
     QPM.append("./src/passes/libQirRemoveBasicBlocksWithSingleNonConditionalBranchInstsPass.so");
 
-	// Run the passes
+    // Run the passes
     QPM.run(module.get(), MAM);
 
-	// Print the result
+    // Print the result
     module->print(outs(), nullptr);
 
     return 0;

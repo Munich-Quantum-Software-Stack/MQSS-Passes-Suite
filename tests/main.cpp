7a071842f06f58cb7190b4801266b3679a48298d
Jorge Echavarria <jorge.echavarria@lrz.de>
2023-08-22 15:25:10 +0000

    Removed unnecessary comments

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,39 +5,29 @@
 using namespace llvm;
 
 int main() {
-    /*PassBuilder             PB;
-    LoopAnalysisManager     LAM;
-    FunctionAnalysisManager FAM;
-    CGSCCAnalysisManager    CGAM;*/
-    ModuleAnalysisManager   MAM; 
+	ModuleAnalysisManager MAM;
+    QirPassManager        QPM;
+    LLVMContext           Context;
+    SMDiagnostic          error;
 
-    // Set up the analysis managers
-    /*PB.registerModuleAnalyses(MAM);
-    PB.registerCGSCCAnalyses(CGAM);
-    PB.registerFunctionAnalyses(FAM);
-    PB.registerLoopAnalyses(LAM);*/
-
-    QirPassManager QPM;
-
-    LLVMContext Context;
-
-    // Step 1: Read the LLVM IR from .ll file
-    SMDiagnostic error;
+   	// Read QIR 
     std::unique_ptr<Module> module = parseIRFile("../benchmarks/bell_state.ll", error, Context);
     if (!module) {
         std::cerr << "Error reading .ll file." << std::endl;
-        error.print("apply_pass", errs());
         return 1;
     }
 
+	// Append passes
     QPM.append("./src/passes/libQirRemoveNonEntrypointFunctionsPass.so");
     QPM.append("./src/passes/libQirGroupingPass.so");
     QPM.append("./src/passes/libQirBarrierBeforeFinalMeasurementsPass.so");
     QPM.append("./src/passes/libQirCXCancellationPass.so");
     QPM.append("./src/passes/libQirRemoveBasicBlocksWithSingleNonConditionalBranchInstsPass.so");
 
+	// Run the passes
     QPM.run(module.get(), MAM);
 
+	// Print the result
     module->print(outs(), nullptr);
 
     return 0;

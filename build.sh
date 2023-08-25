f0b7bbd4088778cf01920b10413351702d992575
Jorge Echavarria <jorge.echavarria@lrz.de>
2023-08-25 13:57:24 +0000

    Minor changes build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 sudo apt install -y cmake llvm libopenmpi-dev
-#rm -rf build/ 2> /dev/null
-rm build/qpm_d 2> /dev/null
+rm -rf build/ 2> /dev/null
+#rm build/qpm_d 2> /dev/null
 mkdir build/ 2> /dev/null
 cd build/
 export CMAKE_PREFIX_PATH=$(llvm-config --libdir)/cmake/llvm

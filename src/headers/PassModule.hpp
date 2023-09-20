3217fcc804ac0562b23a67bb29e8fd7022432b80
Jorge Echavarria <jorge.echavarria@lrz.de>
2023-09-20 13:34:49 +0000

    Minor change

--- src/headers/PassModule.hpp
+++ src/headers/PassModule.hpp
@@ -10,9 +10,3 @@ public:
     virtual ~PassModule() {}
 };
 
-/*class AnalysisFunction {
-public:
-    virtual Result run(Function &function, FunctionAnalysisManager &FAM) = 0;
-    virtual ~AnalysisFunction() {}
-};*/
-

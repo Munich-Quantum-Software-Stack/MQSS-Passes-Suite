700bd17809bfd8a388bad34c7d9259cc4466e2ca
Aleksandra Swierkowska <aleksandra.swierkowska@lrz.de>
2023-10-13 13:18:29 +0000

    Updated draft of QirNullRotationCancellation

--- src/passes/QirNullRotationCancellation.cpp
+++ src/passes/QirNullRotationCancellation.cpp
@@ -3,6 +3,13 @@
 using namespace llvm;
 
 PreservedAnalyses QirNullRotationCancellationPass::run(Module &module, ModuleAnalysisManager &/*MAM*/) {
+    auto& Context = module.getContext();
+
+    const double zeroValue   = 0.0;
+    const double piValue     = 3.14159265;
+    ConstantFP* zeroConstant = ConstantFP::get(Context, APFloat(zeroValue));
+    ConstantFP* piConstant   = ConstantFP::get(Context, APFloat(piValue));
+
     std::unordered_set<std::string> rotationGates = {"__quantum__qis__rx__body", "__quantum__qis__ry__body", "__quantum__qis__rz__body"};
     for (auto &function : module) {
         std::vector<CallInst*> rotationGatesToRemove;
@@ -17,14 +24,23 @@ PreservedAnalyses QirNullRotationCancellationPass::run(Module &module, ModuleAna
                         continue;
                     
                     std::string current_name = current_function->getName().str();
-                    if (rotationGates.find(current_name) != rotationGates.end()) {
-			if (auto *rotation = dyn_cast<ConstantFP>(current_function->getArg(0))) {
-			    errs() << "DEBUG: " << *rotation << '\n';
-                            if (rotation->isZero()) {
-                                rotationGatesToRemove.push_back(current_instruction);
-                                errs() << "\tRedundant rotation found: " << current_name << '\n';
-                            }
+
+	            if (rotationGates.find(current_name) != rotationGates.end()) {
+			Value *argument = current_function->getArg(0);
+			errs() << "DEBUG " << current_name << '\n';
+			if (argument->getType()->isDoubleTy()) {
+                            errs() << "DEBUG first check" << *argument->getType() << '\n';
+			    if (ConstantFP *constFP = dyn_cast<ConstantFP>(argument)) {
+                                errs() << "DEBUG into casting" << '\n';
+				double rotation = constFP->getValueAPF().convertToDouble();
+                                errs() << "DEBUG getting rotation value: " << rotation << '\n';
+                                if (rotation == zeroValue || rotation == 2*piValue) {
+                                    rotationGatesToRemove.push_back(current_instruction);
+                                    errs() << "\tRedundant rotation found: " << current_name << '\n';
+                                }
+			    }
 			}
+			
                     }
                 }
             }

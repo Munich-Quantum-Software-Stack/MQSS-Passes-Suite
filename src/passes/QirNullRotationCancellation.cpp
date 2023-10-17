1066b30ce50f5ed56b845414cca9c15113069918
Aleksandra Swierkowska <aleksandra.swierkowska@lrz.de>
2023-10-17 11:39:21 +0000

    Add working version of QirNullRotationCancellation

--- src/passes/QirNullRotationCancellation.cpp
+++ src/passes/QirNullRotationCancellation.cpp
@@ -5,10 +5,10 @@ using namespace llvm;
 PreservedAnalyses QirNullRotationCancellationPass::run(Module &module, ModuleAnalysisManager &/*MAM*/) {
     auto& Context = module.getContext();
 
-    const double zeroValue   = 0.0;
-    const double piValue     = 3.14159265;
+    const double zeroValue = 0.0;
+    const double twoPiValue = 2 * 3.14159265;
     ConstantFP* zeroConstant = ConstantFP::get(Context, APFloat(zeroValue));
-    ConstantFP* piConstant   = ConstantFP::get(Context, APFloat(piValue));
+    ConstantFP* twoPiConstant = ConstantFP::get(Context, APFloat(twoPiValue));
 
     std::unordered_set<std::string> rotationGates = {"__quantum__qis__rx__body", "__quantum__qis__ry__body", "__quantum__qis__rz__body"};
     for (auto &function : module) {
@@ -26,20 +26,26 @@ PreservedAnalyses QirNullRotationCancellationPass::run(Module &module, ModuleAna
                     std::string current_name = current_function->getName().str();
 
 	            if (rotationGates.find(current_name) != rotationGates.end()) {
-			Value *argument = current_function->getArg(0);
-			errs() << "DEBUG " << current_name << '\n';
-			if (argument->getType()->isDoubleTy()) {
-                            errs() << "DEBUG first check" << *argument->getType() << '\n';
-			    if (ConstantFP *constFP = dyn_cast<ConstantFP>(argument)) {
-                                errs() << "DEBUG into casting" << '\n';
-				double rotation = constFP->getValueAPF().convertToDouble();
-                                errs() << "DEBUG getting rotation value: " << rotation << '\n';
-                                if (rotation == zeroValue || rotation == 2*piValue) {
+		        Value* arg = current_instruction->getArgOperand(0);
+
+			    // Case in which double %zero is an argument
+			    if (LoadInst* ArgsAsInstruction = dyn_cast_or_null<LoadInst>(arg)) {
+			        Value* rotationAngle = ArgsAsInstruction->getPointerOperand();
+				if (GlobalVariable* angleAsAConst = dyn_cast_or_null<GlobalVariable>(rotationAngle)) {
+				    if (ConstantFP* angleFP = dyn_cast_or_null<ConstantFP>(angleAsAConst->getInitializer())) {
+				        if (angleFP->isZero() || angleFP == twoPiConstant) {
+                                    	    rotationGatesToRemove.push_back(current_instruction);
+                                    	    errs() << "\tRedundant rotation found: " << current_name << '\n';
+					}
+				    }
+				}
+			    // Case in which double 0.0 is an argument
+			    } else if (ConstantFP* angleFP = dyn_cast_or_null<ConstantFP>(arg)) {
+                                if (angleFP->isZero() || angleFP == twoPiConstant) {
                                     rotationGatesToRemove.push_back(current_instruction);
                                     errs() << "\tRedundant rotation found: " << current_name << '\n';
-                                }
+				}
 			    }
-			}
 			
                     }
                 }

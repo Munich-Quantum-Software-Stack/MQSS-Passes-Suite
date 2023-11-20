c610b5fd3273565935245ed4c27c5dd4beac4eec
Jorge Echavarria <jorge.echavarria@lrz.de>
2023-11-20 10:16:30 +0000

    Replaced bash shell script for source code formatting with pre-commit hooks

--- src/daemon_d.cpp
+++ src/daemon_d.cpp
@@ -3,12 +3,11 @@
  * @brief TODO
  */
 
+#include "connection_handling.hpp"
 #include "pass_runner/PassRunner.hpp"
 #include "pass_runner/QirPassRunner.hpp"
 #include "scheduler_runner/SchedulerRunner.hpp"
 #include "selector_runner/SelectorRunner.hpp"
-
-#include "connection_handling.hpp"
 
 #include <algorithm>
 #include <chrono>

203eb22bf5cbab7997799e281cb5bfe53ab4408f
Jorge Echavarria <jorge.echavarria@lrz.de>
2024-02-22 13:00:21 +0000

    The QirQMapPass can now parse from LLVM::Module to QC::QuantumComputation

--- src/QirQMap.cpp
+++ src/QirQMap.cpp
@@ -19,12 +19,6 @@ using namespace llvm;
 PreservedAnalyses QirQMapPass::run(
     Module &module, ModuleAnalysisManager & /*MAM*/, QDMI_Device dev)
 {
-    //auto &Context = module.getContext();
-    //StructType *qubitType = StructType::getTypeByName(Context, "Qubit");
-    //PointerType *qubitPtrType = PointerType::getUnqual(qubitType);
-    //ConstantPointerNull *nullQubitPointer =
-    //    ConstantPointerNull::get(qubitPtrType);
-
     auto arch = mqt::createArchitecture(dev);
     auto qc   = qc::QuantumComputation(arch.getNqubits(), arch.getNqubits());
 
@@ -40,52 +34,144 @@ PreservedAnalyses QirQMapPass::run(
                     {
                         auto name = static_cast<std::string>(f->getName().str());
                         
-                        if (name == "__quantum__qis__rx__body")
+                        if (name == "__quantum__qis__rx__body"
+                         || name == "__quantum__qis__ry__body"
+                         || name == "__quantum__qis__rz__body")
                         {
+                            int qubit = -1;
+                            double angle = -1.0;
+
                             Value *farg = call_instr->getArgOperand(0);
-                            Value *qarg = call_instr->getArgOperand(1);
-                            auto *angle = dyn_cast_or_null<ConstantFP>(farg);
 
-                            //if (auto *inttoptr_inst = dyn_cast<IntToPtrInst>(qarg))
-                            //    if (auto *constant_int = dyn_cast<ConstantInt>(inttoptr_inst->getOperand(0)))
-                            //        int arg_value = constant_int->getSExtValue();
+                            if (auto *const_fp = dyn_cast<ConstantFP>(farg))
+                            {
+                                APFloat val = const_fp->getValueAPF();
+                                angle = val.convertToDouble();
+                            }
 
-                            errs() << "\n\tangle: " << angle << "\n";
+                            if (auto *call_instru = dyn_cast<CallInst>(&instruction))
+                            {
+                                Value *qarg = call_instru->getArgOperand(1);
 
-                            qc.rx(0.0, 0);
+                                if (isa<ConstantPointerNull>(qarg)) 
+                                    qubit = 0;
+                                else if(ConstantExpr *constExpr = dyn_cast<ConstantExpr>(qarg))
+                                {
+                                    IntToPtrInst* castInstruction = dyn_cast<IntToPtrInst>(constExpr->getAsInstruction());
+                                    ConstantInt *qubitInt = dyn_cast<ConstantInt>(
+                                    castInstruction->getOperand(0));
+                                    qubit = qubitInt->getSExtValue();
+                                }
+                            }
+
+                            if (angle == -1.0 || qubit == -1)
+                            {
+                                errs() << "   [Pass]................Warning: "
+                                       << "ill-formed gate\n";
+                                continue;
+                            }
+
+                            if (name == "__quantum__qis__rx__body")
+                                qc.rx(angle, qubit);
+                            else if (name == "__quantum__qis__ry__body")
+                                qc.ry(angle, qubit);
+                            else if (name == "__quantum__qis__rz__body")
+                                qc.rz(angle, qubit);
                         }
-                        else if (name == "__quantum__qis__ry__body")
-                            qc.ry(0.0, 0);
-                        else if (name == "__quantum__qis__rz__body")
-                            qc.rz(0.0, 0);
-                        else if (name == "__quantum__qis__cx__body" || name == "__quantum__qis__cnot__body")
-                            qc.cx(0, 1);
-                        else if (name == "__quantum__qis__cz__body")
-                            qc.cz(0, 1);
+                        else if (name == "__quantum__qis__cx__body" 
+                              || name == "__quantum__qis__cnot__body"
+                              || name == "__quantum__qis__cy__body"
+                              || name == "__quantum__qis__cz__body")
+                        {
+                            int qubit_control = -1;
+                            int qubit_target  = -1;
+
+                            if (auto *call_instru = dyn_cast<CallInst>(&instruction))
+                            {
+                                Value *qarg_control = call_instru->getArgOperand(0);
+                                Value *qarg_target  = call_instru->getArgOperand(1);
+
+                                if (isa<ConstantPointerNull>(qarg_control))
+                                    qubit_control = 0;
+                                else if(ConstantExpr *constExpr = dyn_cast<ConstantExpr>(qarg_control))
+                                {
+                                    IntToPtrInst* castInstruction = dyn_cast<IntToPtrInst>(constExpr->getAsInstruction());
+                                    ConstantInt *qubitInt = dyn_cast<ConstantInt>(
+                                    castInstruction->getOperand(0));
+                                    qubit_control = qubitInt->getSExtValue();
+                                }
+
+                                if (isa<ConstantPointerNull>(qarg_target))
+                                    qubit_target = 0;
+                                else if(ConstantExpr *constExpr = dyn_cast<ConstantExpr>(qarg_target))
+                                {
+                                    IntToPtrInst* castInstruction = dyn_cast<IntToPtrInst>(constExpr->getAsInstruction());
+                                    ConstantInt *qubitInt = dyn_cast<ConstantInt>(
+                                    castInstruction->getOperand(0));
+                                    qubit_target = qubitInt->getSExtValue();
+                                }
+                            }
+
+                            if (qubit_control == -1 || qubit_target == -1)
+                            {
+                                errs() << "   [Pass]................Warning: "
+                                       << "ill-formed gate\n";
+                                continue;
+                            }
+
+                            if (name == "__quantum__qis__cx__body" || name == "__quantum__qis__cnot__body")
+                                qc.cx(qubit_control, qubit_target);
+                            else if (name == "__quantum__qis__cy__body")
+                                qc.cy(qubit_control, qubit_target);
+                            else if (name == "__quantum__qis__cz__body")
+                                qc.cz(qubit_control, qubit_target);
+                        }
                         else if (name == "__quantum__qis__mz__body")
-                            qc.measure(0, 0);
-                        //else if (name == "__quantum__qis__s_adj__body")
-                        //    qc.sdg(0);
-                        else if (name == "__quantum__qis__s__body")
-                            qc.s(0);
-                        else if (name == "__quantum__qis__t__body")
-                            qc.t(0);
-                        else if (name == "__quantum__qis__x__body")
-                            qc.x(0);
-                        else if (name == "__quantum__qis__y__body")
-                            qc.y(0);
-                        else if (name == "__quantum__qis__z__body")
-                            qc.z(0);
-                        else if (name == "__quantum__qis__h__body")
-                            qc.h(0);
-                        //else if (name == "__quantum__qis__ccx__body")
-                        //    qc.ccx(0);
-                        else if (name == "__quantum__qis__reset__body")
-                            qc.reset(0);
-                        //else if (name == "__quantum__qis__swap__body") //TODO Not supported by QMAP?
-                        //    qc.swap(0, 1);
-                        //else if (name == "__quantum__qis__t_adj__body")
-                        //    qc.t(0);
+                            ; // QMap inserts a measure all instruction
+                        else if (name == "__quantum__qis__s__body"
+                              || name == "__quantum__qis__t__body"
+                              || name == "__quantum__qis__x__body"
+                              || name == "__quantum__qis__y__body"
+                              || name == "__quantum__qis__z__body"
+                              || name == "__quantum__qis__h__body")
+                        {
+                            int qubit = -1;
+
+                            if (auto *call_instru = dyn_cast<CallInst>(&instruction))
+                            {
+                                Value *qarg = call_instru->getArgOperand(0);
+
+                                if (isa<ConstantPointerNull>(qarg))
+                                    qubit = 0;
+                                else if(ConstantExpr *constExpr = dyn_cast<ConstantExpr>(qarg))
+                                {
+                                    IntToPtrInst* castInstruction = dyn_cast<IntToPtrInst>(constExpr->getAsInstruction());
+                                    ConstantInt *qubitInt = dyn_cast<ConstantInt>(
+                                    castInstruction->getOperand(0));
+                                    qubit = qubitInt->getSExtValue();
+                                }
+                            }
+
+                            if (qubit == -1)
+                            {
+                                errs() << "   [Pass]................Warning: "
+                                       << "ill-formed gate\n";
+                                continue;
+                            }
+
+                            if (name == "__quantum__qis__s__body")
+                                qc.s(qubit);
+                            else if (name == "__quantum__qis__t__body")
+                                qc.t(qubit);
+                            else if (name == "__quantum__qis__x__body")
+                                qc.x(qubit);
+                            else if (name == "__quantum__qis__y__body")
+                                qc.y(qubit);
+                            else if (name == "__quantum__qis__z__body")
+                                qc.z(qubit);
+                            else if (name == "__quantum__qis__h__body")
+                                qc.h(qubit);
+                        }
                         else
                         {
                             errs() << "   [Pass]................Warning: "

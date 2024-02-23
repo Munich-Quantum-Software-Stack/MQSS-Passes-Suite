c3db2fbdb755bd88497c287897f65a95af3053cb
martin <martin.knudsen@wmi.badw.de>
2024-02-23 10:57:47 +0100

    change all cmake to wmi-backend

--- cmake/Findqrm.cmake
+++ cmake/Findqrm.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     qrm
     GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/QRM.git
-    GIT_TAG develop
+    GIT_TAG wmi-backend
 )
 
 FetchContent_MakeAvailable(qrm)

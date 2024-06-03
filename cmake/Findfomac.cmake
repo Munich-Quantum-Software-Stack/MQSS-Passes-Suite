91f11dff47604443af957419ed6c07b02603411e
di93coc <kayaercument95@gmail.com>
2024-06-03 07:53:44 +0000

    Remove metadata dependency and move the PassModule from QRM

--- cmake/Findfomac.cmake
+++ cmake/Findfomac.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     fomac
     GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/FoMaC.git
-    GIT_TAG develop
+    GIT_TAG CleanUp
 )
 
 FetchContent_MakeAvailable(fomac)

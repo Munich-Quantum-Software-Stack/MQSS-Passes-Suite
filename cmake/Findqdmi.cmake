b6e0864ecb2d6f3e495379cc7de1b6931cfda48a
Martin Letras <di35hec@badwlrz-cl14642>
2024-12-05 18:32:06 +0000

    Including find package for qdmi and qinfo

---
+++ cmake/Findqdmi.cmake
@@ -0,0 +1,13 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    qdmi
+    GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/QDMI.git
+    GIT_TAG v0.1
+)
+
+FetchContent_MakeAvailable(qdmi)
+
+FetchContent_GetProperties(qdmi)
+
+set(QDMI_INCLUDE_DIRS "${qdmi_SOURCE_DIR}/include")

397c370b5f427e1fcfb05aa1afad891f7a4dad84
Martin Letras <di35hec@badwlrz-cl14642>
2024-12-13 13:09:25 +0000

    Changing signature of createQuakeQMapPas -> now receives the architecture, and the settings to be applied to the mapper

--- include/Passes.hpp
+++ include/Passes.hpp
@@ -9,6 +9,7 @@
 #include "mlir/Pass/PassRegistry.h"
 #include "llvm/Support/raw_ostream.h"
 #include "qdmi.h"
+#include "sc/heuristic/HeuristicMapper.hpp"
 
 #define CUDAQ_PREFIX_FUNCTION "__nvqpp__mlirgen__"
 
@@ -16,7 +17,8 @@ namespace mqss::opt{
 
 std::unique_ptr<mlir::Pass> createCustomExamplePass();
 std::unique_ptr<mlir::Pass> createPrintQuakeGatesPass(llvm::raw_string_ostream &ostream);
-std::unique_ptr<mlir::Pass> createQuakeQMapPass(llvm::raw_string_ostream &ostream);
+
+std::unique_ptr<mlir::Pass> createQuakeQMapPass(Architecture &architecture, const Configuration &settings);
 
 } // end namespace
 #endif // PASSES_H

a4a67243a846cb82e6abb118cab8ad30cadb49b8
Martin Letras <di35hec@badwlrz-cl14642>
2025-01-13 17:33:03 +0000

    Defining a commuteOperation, more general. Uses templates and receives the number of controls and targets of the pattern to find

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -146,5 +146,59 @@ namespace mqss::utils{
       }
       return nullptr; // No matching previous operation found
   }
+ 
+  // Finds the pattern composed of T2, T1 and commute them to T1, T2
+
+  template <typename T1, typename T2>
+  inline void commuteOperation(mlir::Operation *currentOp,
+                               int nCtrlsOp1,
+                               int nTgtsOp1,
+                               int nCtrlsOp2,
+                               int nTgtsOp2){
+    auto currentGate = dyn_cast_or_null<T2>(*currentOp);
+    if (!currentGate)
+      return;
+    // check that the current gate is compliant with the number of controls and targets
+    if (currentGate.getControls().size() != nCtrlsOp2 ||
+        currentGate.getTargets().size() != nTgtsOp2)
+      return;
+    // get the previous operation to check the swap pattern
+    auto prevOp = getPreviousOperationOnTarget(currentGate, currentGate.getTargets()[0]);
+    auto previousGate = dyn_cast_or_null<T1>(prevOp);
+    if (!previousGate)
+      return;
+    // check that the previous gate is compliant with the number of controls and targets
+    if (previousGate.getControls().size() != nCtrlsOp1 ||
+        previousGate.getTargets().size() != nTgtsOp1)
+      return;  // check both targets are the same
+    int targetPrev = extractIndexFromQuakeExtractRefOp(previousGate.getTargets()[0].getDefiningOp());
+    int targetCurr = extractIndexFromQuakeExtractRefOp(currentGate.getTargets()[0].getDefiningOp());
+    if (targetPrev != targetCurr)
+      return;
+    #ifdef DEBUG
+      llvm::outs() << "Current Operation: ";
+      currentGate->print(llvm::outs());
+      llvm::outs() << "\n";
+      llvm::outs() << "Previous Operation: ";
+      previousGate->print(llvm::outs());
+      llvm::outs() << "\n";
+    #endif
+    // At this point, I should de able to do the commutation
+    // Swap the two operations by cloning them in reverse order.
+    mlir::IRRewriter rewriter(currentGate->getContext());
+    rewriter.setInsertionPointAfter(currentGate);
+    auto newGate = rewriter.create<T1>(previousGate.getLoc(),
+                                       previousGate.isAdj(),
+                                       previousGate.getParameters(),
+                                       previousGate.getControls(),
+                                       previousGate.getTargets());
+    rewriter.setInsertionPoint(newGate);
+    rewriter.create<T2>(currentGate.getLoc(), currentGate.isAdj(),
+                        currentGate.getParameters(), currentGate.getControls(),
+                        currentGate.getTargets());
+    // Erase the original operations
+    rewriter.eraseOp(currentGate);
+    rewriter.eraseOp(previousGate);
+  }
 } // end namespace
 #endif // UTILS_H

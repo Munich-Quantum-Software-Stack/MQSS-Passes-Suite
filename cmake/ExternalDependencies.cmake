84316d1913d4791b050632c1096cb82eda7c88d3
Martin Letras <di35hec@badwlrz-cl14642>
2025-01-22 13:02:12 +0000

    Tag TODO to put the licence

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -5,7 +5,7 @@
 # "License"); you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# https://github.com/Munich-Quantum-Software-Stack/QDMI/blob/develop/LICENSE
+# TODO: LICENSE
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT

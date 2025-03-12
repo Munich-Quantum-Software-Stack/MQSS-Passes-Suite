74974260ddcb77de3f3c2156190e450fd6ed699a
Martin Letras <di35hec@badwlrz-cl14642>
2025-03-12 14:40:35 +0000

    After including pre-commit hooks

--- cmake/Findqinfo.cmake
+++ cmake/Findqinfo.cmake
@@ -1,9 +1,8 @@
 include(FetchContent)
 
 FetchContent_Declare(
-    qinfo
-    GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/QInfo.git
-    GIT_TAG develop
-)
+  qinfo
+  GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/QInfo.git
+  GIT_TAG develop)
 
 FetchContent_MakeAvailable(qinfo)

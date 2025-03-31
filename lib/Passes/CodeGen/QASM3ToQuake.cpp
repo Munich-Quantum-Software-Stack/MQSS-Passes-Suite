00baabc668c1670de49f12efb4098218aa1df9fb
Martin Letras <di35hec@badwlrz-cl14642>
2025-03-31 09:11:30 +0000

    🔥 Returning to keep these passes to operate at func::FuncOp Level

--- lib/Passes/CodeGen/QASM3ToQuake.cpp
+++ lib/Passes/CodeGen/QASM3ToQuake.cpp
@@ -814,7 +814,7 @@ void parseAndInsertMeasurements(
 }
 
 class QASM3ToQuake
-    : public PassWrapper<QASM3ToQuake, OperationPass<mlir::ModuleOp>> {
+    : public PassWrapper<QASM3ToQuake, OperationPass<func::FuncOp>> {
 public:
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(QASM3ToQuake)
 

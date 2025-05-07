52cbe5fda447dab0b06814606cfb33a9e652c2b8
Martin Letras <di35hec@badwlrz-cl14642>
2025-05-07 06:18:26 +0000

    📝 Including comments for doxygen documentation

--- include/Interfaces/QASMToQuake.hpp
+++ include/Interfaces/QASMToQuake.hpp
@@ -15,18 +15,20 @@ License for the specific language governing permissions and limitations under
 the License.
 
 SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
--------------------------------------------------------------------------
+******************************************************************************
   author Martin Letras
   date   May 2025
   version 1.0
-  brief
-  This header defines a set of functions utilized to parse QASM circuits to
-  Quake/MLIR modules.
-
-*******************************************************************************
-* This source code and the accompanying materials are made available under    *
-* the terms of the Apache License 2.0 which accompanies this distribution.    *
 ******************************************************************************/
+/** @file
+ * @brief
+ * @details This header defines a set of functions utilized to parse QASM
+ * circuits to MLIR/Quake modules.
+ *
+ * @par
+ * This header file is used by the QASM3ToQuakePass to perform the conversion of
+ * QASM programs to MLIR/Quake modules.
+ */
 
 #pragma once
 
@@ -39,13 +41,34 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #include "mlir/Pass/PassRegistry.h"
 
 using namespace mlir;
+/**
+ * @typedef QASMVectorToQuakeVector
+ * @brief The `QASMVectorToQuakeVector` is a map of type
+ * `std::unordered_map<std::string, mlir::Value>`. The `key` is a string
+ * corresponding to a quantum vector declared in the QASM program and the
+ * `value` corresponds to an `mlir::Value` associated to a created and inserted
+ * `quake::veq`.
+ */
 using QASMVectorToQuakeVector = std::unordered_map<std::string, mlir::Value>;
 
 namespace mqss::interfaces {
 
-// Given a gateId as a string, this functions inserts the corresponding Quake
-// gate in the given builder. Parameters such as loc, vecParams (vector of
-// arguments, i.e.,angles) vecControls and vecTargets must be given too.
+/**
+ * @brief Given a gate type as a string, this functions inserts the
+ corresponding Quake gate in the given builder.
+   @details This method inserts a quake operation into an MLIR module associated
+ with the builder passed as parameter.
+    @param[in] gateId is a string specifying the type of the quantum gate to be
+ inserted.
+    @param[out] builder is an `OpBuilder` object associated with a MLIR module.
+ It is used to insert new instructions to the corresponding MLIR module.
+    @param[in] loc is the location of the new inserted instruction.
+    @param[in] vecParams is the vector of arguments of the gate to be inserted,
+ e.g., rotation angles in rotation gates.
+    @param[in] vecControls is the vector of control qubits.
+    @param[in] vecTargets is the vector of target qubits.
+    @param[in] adj determines is the gate is an adjoint operation.
+*/
 void insertQASMGateIntoQuakeModule(std::string gateId, OpBuilder &builder,
                                    Location loc,
                                    std::vector<mlir::Value> vecParams,
@@ -53,28 +76,94 @@ void insertQASMGateIntoQuakeModule(std::string gateId, OpBuilder &builder,
                                    std::vector<mlir::Value> vecTargets,
                                    bool adj);
 
-// Function to determine if a gate is a multi-qubit gate with implicit controls
+/**
+ * @brief Given a gate type as a string, this functions checks if the given gate
+ type has control outputs.
+   @details
+    @param[in] gateType is a string specifying the type of a quantum gate.
+    @return `true` if a gate is a multi-qubit gate with implicit controls.
+*/
 bool isMultiQubitGate(const std::string &gateType);
 
-// Function to get the number of controls for a gate
+/**
+ * @brief Given a gate type as a string, this functions returns the number of
+ control outputs associated with the given gate type.
+   @details
+    @param[in] gateType is a string specifying the type a quantum gate.
+    @return the number of control outputs of the give gate type.
+*/
 size_t getNumControls(const std::string &gateType);
 
-// This function returns the set of quantum registers declared in a given QASM
-// program.
+/**
+ * @brief This function return the quantum vectors and its order, given a
+ Abstract Syntax Tree of a QASM program.
+   @details This function receives an AST of QASM program, and create a
+ `quake::veq` for each quantum register declared in the QASM program. Moreover,
+ returns the order of appearance of each quantum register.
+    @param[in] program is the AST of a QASM program.
+    @param[out] builder is an `OpBuilder` object associated with a MLIR module.
+ It is used to insert new instructions (quantum registers) to the corresponding
+ MLIR module.
+    @param[in] loc is the location of the new inserted instruction (quantum
+ registers).
+    @param[in] inOp is the `return` operation in the module associated to the
+ builder. New instructions are inserted before the`return` operation.
+    @return a tuple composed of `QASMVectorToQuakeVector` and
+ `std::vector<std::pair<std::string, int>>`. The `QASMVectorToQuakeVector` is a
+ map of type `std::unordered_map<std::string, mlir::Value>`. The `key` is a
+ string corresponding to a quantum vector declared in the QASM program and the
+ `value` corresponds to an `mlir::Value` associated to a created and inserted
+ `quake::veq`. The second argument of the tuple is a vector that preserves the
+ order of the inserted `quake::veq`, each entry is pair storing the quantum
+ vector id, and the size of the quantum vector.
+*/
 std::tuple<QASMVectorToQuakeVector, std::vector<std::pair<std::string, int>>>
 insertAllocatedQubits(
     const std::vector<std::shared_ptr<qasm3::Statement>> &program,
     OpBuilder &builder, Location loc, mlir::Operation *inOp);
 
+/**
+ * @brief Function that evaluates a numeric expression in the AST.
+   @details
+    @param[in] expr is as qasm3 numeric expression.
+    @return a double value associated with the input numeric expression.
+*/
 double evaluateExpression(const std::shared_ptr<qasm3::Expression> &expr);
 
-// Function that inserts a QASM gate into a MLIR/Quake module
+/**
+ * @brief This function inserts a gate into a MLIR/Quake module.
+   @details The QASM to Quake parser invokes this function to insert each gate
+ in the AST.
+    @param[in] gateCall is qasm3 gate declaration.
+    @param[out] builder is an `OpBuilder` object associated with a MLIR module.
+ It is used to insert new instructions (quantum registers) to the corresponding
+ MLIR module.
+    @param[in] loc is the location of the new inserted instruction (quantum
+ registers).
+    @param[in] inOp is the `return` operation in the module associated to the
+ builder. New instructions are inserted before the`return` operation.
+    @param[in] QASMToVectors map that returns a `mlir::Value` associated to a
+ declared quantum vector, given the QASM quantum vector declaration.
+*/
 void insertGate(const std::shared_ptr<qasm3::GateCallStatement> &gateCall,
                 OpBuilder &builder, Location loc, mlir::Operation *inOp,
                 QASMVectorToQuakeVector QASMToVectors);
 
-// Function that parses a given AST/QASM and inserts measurements into a
-// MLIR/Quake
+/** TODO
+ * @brief This function inserts measurements into a MLIR/Quake module.
+   @details The QASM to Quake parser invokes this function to insert
+ measurements into a given MLIR/Quake module.
+    @param[in] statements is qasm3 measurements declarations.
+    @param[out] builder is an `OpBuilder` object associated with a MLIR module.
+ It is used to insert new instructions (quantum registers) to the corresponding
+ MLIR module.
+    @param[in] loc is the location of the new inserted instruction (quantum
+ registers).
+    @param[in] inOp is the `return` operation in the module associated to the
+ builder. New instructions are inserted before the`return` operation.
+    @param[in] QASMToVectors map that returns a `mlir::Value` associated to a
+ declared quantum vector, given the QASM quantum vector declaration.
+*/
 void parseAndInsertMeasurements(
     const std::vector<std::shared_ptr<qasm3::Statement>> &statements,
     OpBuilder &builder, Location loc, mlir::Operation *inOp,

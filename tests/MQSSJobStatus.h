01f3563538ba4064f042b8faf74ebe8c30accca9
Martin Letras <di35hec@badwlrz-cl14642>
2025-07-30 15:43:27 +0000

    ✨ Including rest client to communicate to MQSS

---
+++ tests/MQSSJobStatus.h
@@ -0,0 +1,54 @@
+/* This code and any associated documentation is provided "as is"
+
+Copyright 2025 Munich Quantum Software Stack Project
+
+Licensed under the Apache License, Version 2.0 with LLVM Exceptions (the
+"License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://github.com/Munich-Quantum-Software-Stack/MQSS-CUDAQ-Adapter/tree/develop?tab=readme-ov-file#
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations under
+the License.
+
+SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+-------------------------------------------------------------------------
+  author Martin Letras
+  @date   November 2024
+  @version 1.0
+  @ brief
+
+  Enum that defines the status of a job returned by the MQSS via the MQP.
+
+*******************************************************************************
+* This source code and the accompanying materials are made available under    *
+* the terms of the Apache License 2.0 which accompanies this distribution.    *
+******************************************************************************/
+
+#ifndef MQSSJOBSTATUS_H
+#define MQSSJOBSTATUS_H
+
+// Define an enumeration for JobStatus
+namespace cudaq::mqss {
+enum class JobStatus { PENDING, WAITING, FAILED, COMPLETED, CANCELLED };
+
+const std::string jobStatusToString(JobStatus jobStatus) {
+  switch (jobStatus) {
+  case JobStatus::PENDING:
+    return "PENDING";
+  case JobStatus::WAITING:
+    return "WAITING";
+  case JobStatus::FAILED:
+    return "FAILED";
+  case JobStatus::COMPLETED:
+    return "COMPLETED";
+  case JobStatus::CANCELLED:
+    return "CANCELLED";
+  }
+  return "UNKNOWN";
+}
+} // namespace cudaq::mqss
+#endif // MQSS_JOBSTATUS_H
---
+++ tests/RestClient.cpp
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+#include "RestClient.h"
+
+#include <iostream>
+// #include "cudaq/utils/cudaq_utils.h"
+#include <cpr/cpr.h>
+#include <fmt/core.h>
+// #include <zlib.h>
+
+constexpr long validHttpCode = 205;
+
+RestClient::RestClient() : sslOptions(std::make_unique<cpr::SslOptions>()) {
+  auto caInfo = [&]() -> std::string {
+    if (auto *curlCABundleStr = getenv("CURL_CA_BUNDLE")) {
+      if (std::filesystem::exists(curlCABundleStr))
+        return curlCABundleStr;
+      else
+        std::cout << curlCABundleStr
+                  << " does not exist. Will fall back on CUDA-Q installed certs"
+                  << std::endl;
+    }
+    /*std::filesystem::path cudaqLibPath{cudaq::getCUDAQLibraryPath()};
+    auto certPath = cudaqLibPath.parent_path().parent_path() / "cacert.pem";
+    if (std::filesystem::exists(certPath))
+      return certPath.string();
+    std::cout << certPath.string() <<
+        " does not exist, so we will rely on CURL finding the correct "
+        "certificate authority bundles. If this does not work, try setting "
+        "the CURL_CA_BUNDLE environment variable to a valid path to a CA "
+        "Bundle file, like one downloaded from here: "
+        "https://curl.se/ca/cacert.pem." << std::endl;*/
+    return "";
+  }();
+
+  if (!caInfo.empty())
+    sslOptions->SetOption(cpr::ssl::CaInfo(std::move(caInfo)));
+}
+
+// Must define this in the cpp file instead of the header file
+// because CPR headers aren't included in RestClient.h.
+RestClient::~RestClient() = default;
+
+nlohmann::json RestClient::post(const std::string_view remoteUrl,
+                                const std::string_view path,
+                                nlohmann::json &post,
+                                std::map<std::string, std::string> &headers,
+                                bool enableLogging, bool enableSsl) {
+  if (headers.empty())
+    headers.insert(std::make_pair("Content-type", "application/json"));
+
+  cpr::Header cprHeaders;
+  for (auto &kv : headers)
+    cprHeaders.insert({kv.first, kv.second});
+
+  // Allow caller to disable logging for things like passwords/tokens
+#ifdef DEBUG
+  std::cout << "Posting to " << remoteUrl << "/" << path
+            << " with data = " << post.dump() << std::endl;
+#endif
+  auto actualPath = std::string(remoteUrl) + std::string(path);
+  auto r = cpr::Post(cpr::Url{actualPath}, cpr::Body(post.dump()), cprHeaders,
+                     cpr::VerifySsl(enableSsl), *sslOptions);
+
+  if (r.status_code > validHttpCode || r.status_code == 0)
+    throw std::runtime_error("HTTP POST Error - status code " +
+                             std::to_string(r.status_code) + ": " +
+                             r.error.message + ": " + r.text);
+
+  return nlohmann::json::parse(r.text);
+}
+
+void RestClient::put(const std::string_view remoteUrl,
+                     const std::string_view path, nlohmann::json &putData,
+                     std::map<std::string, std::string> &headers,
+                     bool enableLogging, bool enableSsl) {
+  if (headers.empty())
+    headers.insert(std::make_pair("Content-type", "application/json"));
+
+  cpr::Header cprHeaders;
+  for (auto &kv : headers)
+    cprHeaders.insert({kv.first, kv.second});
+
+  // Allow caller to disable logging for things like passwords/tokens
+#ifdef DEBUG
+  std::cout << "Putting to" << remoteUrl << "/" << path
+            << " with data = " << putData.dump() << std::endl;
+#endif
+  auto actualPath = std::string(remoteUrl) + std::string(path);
+  auto r = cpr::Put(cpr::Url{actualPath}, cpr::Body(putData.dump()), cprHeaders,
+                    cpr::VerifySsl(enableSsl), *sslOptions);
+
+  if (r.status_code > validHttpCode || r.status_code == 0)
+    throw std::runtime_error("HTTP PUT Error - status code " +
+                             std::to_string(r.status_code) + ": " +
+                             r.error.message + ": " + r.text);
+}
+
+nlohmann::json RestClient::get(const std::string_view remoteUrl,
+                               const std::string_view path,
+                               std::map<std::string, std::string> &headers,
+                               bool enableSsl) {
+  if (headers.empty())
+    headers.insert(std::make_pair("Content-type", "application/json"));
+
+  cpr::Header cprHeaders;
+  for (auto &kv : headers)
+    cprHeaders.insert({kv.first, kv.second});
+
+  cpr::Parameters cprParams;
+  auto actualPath = std::string(remoteUrl) + std::string(path);
+  auto r = cpr::Get(cpr::Url{actualPath}, cprHeaders, cprParams,
+                    cpr::VerifySsl(enableSsl), *sslOptions);
+
+  if (r.status_code > validHttpCode || r.status_code == 0)
+    throw std::runtime_error("HTTP GET Error - status code " +
+                             std::to_string(r.status_code) + ": " +
+                             r.error.message + ": " + r.text);
+
+  return nlohmann::json::parse(r.text);
+}
+
+void RestClient::del(const std::string_view remoteUrl,
+                     const std::string_view path,
+                     std::map<std::string, std::string> &headers,
+                     bool enableLogging, bool enableSsl) {
+  cpr::Header cprHeaders;
+  for (auto &kv : headers)
+    cprHeaders.insert({kv.first, kv.second});
+
+  cpr::Parameters cprParams;
+  auto actualPath = std::string(remoteUrl) + std::string(path);
+#ifdef DEBUG
+  std::cout << "Delete resource at path" << remoteUrl << "/" << path
+            << std::endl;
+#endif
+  auto r = cpr::Delete(cpr::Url{actualPath}, cprHeaders, cprParams,
+                       cpr::VerifySsl(enableSsl), *sslOptions);
+
+  if (r.status_code > validHttpCode || r.status_code == 0)
+    throw std::runtime_error("HTTP DELETE Error - status code " +
+                             std::to_string(r.status_code) + ": " +
+                             r.error.message + ": " + r.text);
+}
+
+void RestClient::download(const std::string_view remoteUrl,
+                          const std::string &filePath, bool enableLogging,
+                          bool enableSsl) {
+  auto r = cpr::Get(cpr::Url{std::string(remoteUrl)}, cpr::Header{},
+                    cpr::Parameters{}, cpr::VerifySsl(enableSsl), *sslOptions);
+
+  if (r.status_code > validHttpCode || r.status_code == 0)
+    throw std::runtime_error("HTTP Download Error - status code " +
+                             std::to_string(r.status_code) + ": " +
+                             r.error.message + ": " + r.text);
+
+#ifdef DEBUG
+  std::cout << "Downloading" << r.text.size() << "bytes from" << remoteUrl
+            << "to file" << filePath << std::endl;
+#endif
+
+  try {
+    // Write the downloaded content to file.
+    std::ofstream outfile(filePath, std::ofstream::binary | std::ios::out);
+    outfile.write(r.text.c_str(), r.text.size());
+    outfile.close();
+  } catch (std::exception &e) {
+    // Rethrow it with a descriptive message
+    throw std::runtime_error(fmt::format(
+        "Failed to write downloaded contents to file {}. Exception: {}.",
+        filePath, e.what()));
+  }
+}
